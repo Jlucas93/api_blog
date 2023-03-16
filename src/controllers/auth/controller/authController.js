794f72da8ac95ace4ceed0f260fd776e5a8e1fb3
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2023-03-16 14:26:57 -0300

    updaet

--- src/controllers/auth/controller/authController.js
+++ src/controllers/auth/controller/authController.js
@@ -13,6 +13,7 @@ class AuthController {
       return res.status(200).json(result)
     } catch (error) {
       console.error(error)
+      
       return res.status(500).json({ error: "Internal Server Error" })
     }
   }
